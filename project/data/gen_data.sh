3b10a40070f064a0fa661262559cb5fd7fec2df6
Stanislav Arnaudov <stanislav_ts@abv.bg>
2019-10-08 00:03:56 +0200

    Adding support for custom -np argument for the mpirun command

--- project/data/gen_data.sh
+++ project/data/gen_data.sh
@@ -5,7 +5,11 @@ CONFIG_FILE=$(realpath $2)
 GEOMETRY_DATA="$(realpath $3)/"
 ROOT_DATA_DIR=$(realpath $4)
 
-FLOW_EXEC="/home/arnaud/code_sys/Hiflow3_2.0/build/examples/flow/flow_tutorial"
+# FLOW_EXEC="/home/arnaud/code_sys/Hiflow3_2.0/build/examples/flow/flow_tutorial"
+FLOW_EXEC=${FLOW_EXEC:-"/home/arnaud/code_sys/Hiflow3_2.0/build/examples/flow/flow_tutorial"}
+MPI_NP=${MPI_NP:-4}
+
+
 
 set_params_f()
 {
@@ -18,20 +22,20 @@ set_params_f()
 # ec="echo"
 # eval "${ec} 213"
 
+echo "Hiflow executable: " $FLOW_EXEC
+echo "MPI workers count:" $MPI_NP
 mkdir -p ${ROOT_DATA_DIR}
 
 count=1
 while IFS=" " read -r visc dens inflow prefix
 do
     output=${prefix}_$count
-
     sed -i -e "s#<OutputPrefix>.*</OutputPrefix>#<OutputPrefix>${output}</OutputPrefix>#g" ${CONFIG_FILE}
-    
     echo "Running simulation: ${output}"
     set_params_f $visc $dens $inflow
     mkdir -p "${ROOT_DATA_DIR}/${output}"
     cd "${ROOT_DATA_DIR}/${output}"
-    # mpirun -np 4 ${FLOW_EXEC} "${CONFIG_FILE}" "${GEOMETRY_DATA}"
+    mpirun -np ${MPI_NP} ${FLOW_EXEC} "${CONFIG_FILE}" "${GEOMETRY_DATA}"
     cd - > /dev/null
     count=`expr $count + 1`
 done < <(tail -n +2 ${PARAMS_FILE} | sed -e 's/\,/ /g') 
