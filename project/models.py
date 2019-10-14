29406f4d2931a68f981eaa1e91a5b26baea41a1a
Arnaudov, Stanislav Tomov <stanislav.arnaudov@iosb.fraunhofer.de>
2019-10-14 18:30:00 +0200

    Writing some of the networks code

--- project/models.py
+++ project/models.py
@@ -1,8 +1,214 @@
 #!/usr/bin/env python
 
+import torch
+import torch.nn as nn
+from torch.nn import init
+import functools
+from torch.optim import lr_scheduler
+
+
 
 
 def update_learning_rate(scheduler, optimizer):
     scheduler.step()
     lr = optimizer.param_groups[0]['lr']
     print('--learning rate = %.7f' % lr)
+
+
+def get_norm_layer(norm_type='instance'):
+    if norm_type == 'batch':
+        norm_layer = functools.partial(nn.BatchNorm2d, affine=True)
+    elif norm_type == 'instance':
+        norm_layer = functools.partial(nn.InstanceNorm2d, affine=False, track_running_stats=False)
+    else:
+        raise NotImplementedError('normalization layer [%s] is not found' % norm_type)
+    return norm_layer
+
+
+def init_weights(net, gain=0.02):
+    def init_func(m):
+        classname = m.__class__.__name__
+        if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
+            init.xavier_normal_(m.weight.data, gain=gain)
+
+            if hasattr(m, 'bias') and m.bias is not None:
+                init.constant_(m.bias.data, 0.0)
+            elif classname.find('BatchNorm2d') != -1:
+                init.normal_(m.weight.data, 1.0, gain)
+                init.constant_(m.bias.data, 0.0)
+
+    print('--Initialize network\'s weights with {}'.format('xavier'))
+    net.apply(init_func)
+
+
+def init_net(net, init_gain=0.02, gpu_id='cuda:0'):
+    net.to(gpu_id)
+    init_weights(net, gain=init_gain)
+    return net
+
+
+def define_G(input_nc, output_nc, ngf, norm='batch', use_dropout=True, init_gain=0.02, gpu_id='cuda:0'):
+    net = None
+    
+    norm_layer = get_norm_layer(norm_type=norm)
+    
+    net = ResnetGenerator(input_nc, output_nc, ngf, norm_layer=norm_layer, use_dropout=use_dropout, n_blocks=9)
+
+    return init_net(net, init_gain, gpu_id)
+
+
+
+
+
+class ResnetGenerator(nn.Module):
+    
+    def __init__(self, input_nc, output_nc, ngf=64, norm_layer=nn.BatchNorm2d, use_dropout=True, n_blocks=4, padding_type='reflect'):
+        super(ResnetGenerator, self).__init__()
+        
+        self.input_nc = input_nc
+        self.output_nc = output_nc
+        self.ngf = ngf
+
+        if type(norm_layer) == functools.partial:
+            use_bias = norm_layer.func == nn.InstanceNorm2d
+        else:
+            use_bias = norm_layer == nn.InstanceNorm2d
+
+        self.inc = Inconv(input_nc, ngf, norm_layer, use_bias)
+
+        self.down1 = Down(ngf, ngf * 2, norm_layer, use_bias)
+        self.down2 = Down(ngf * 2, ngf * 4, norm_layer, use_bias)
+
+        model = []
+        for i in range(n_blocks):
+            model += [ResBlock(ngf * 4, padding_type=padding_type, norm_layer=norm_layer, use_dropout=use_dropout, use_bias=use_bias)]
+        self.resblocks = nn.Sequential(*model)
+
+        self.up1 = Up(ngf * 4, ngf * 2, norm_layer, use_bias)
+        self.up2 = Up(ngf * 2, ngf, norm_layer, use_bias)
+
+        self.outc = Outconv(ngf, output_nc)
+
+    def forward(self, input):
+        out = {}
+        out['in'] = self.inc(input)
+        out['d1'] = self.down1(out['in'])
+        out['d2'] = self.down2(out['d1'])
+        out['bottle'] = self.resblocks(out['d2'])
+        out['u1'] = self.up1(out['bottle'])
+        out['u2'] = self.up2(out['u1'])
+        return self.outc(out['u2'])
+
+
+class Inconv(nn.Module):
+    
+    def __init__(self, in_ch, out_ch, norm_layer, use_bias):
+        super(Inconv, self).__init__()
+
+        self.inconv = nn.Sequential(
+            nn.ReflectionPad2d(3),
+            nn.Conv2d(in_ch, out_ch, kernel_size=7, padding=0,
+                      bias=use_bias),
+            norm_layer(out_ch),
+            nn.ReLU(True)
+        )
+
+    def forward(self, x):
+        x = self.inconv(x)
+        return x
+
+
+class Outconv(nn.Module):
+    
+    def __init__(self, in_ch, out_ch):
+        super(Outconv, self).__init__()
+        self.outconv = nn.Sequential(
+            nn.ReflectionPad2d(3),
+            nn.Conv2d(in_ch, out_ch, kernel_size=7, padding=0),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        x = self.outconv(x)
+        return x
+
+
+class Up(nn.Module):
+
+    def __init__(self, in_ch, out_ch, norm_layer, use_bias):
+        super(Up, self).__init__()
+        self.up = nn.Sequential(
+            nn.ConvTranspose2d(in_ch, out_ch,
+                               kernel_size=3, stride=2,
+                               padding=1, output_padding=1,
+                               bias=use_bias),
+            norm_layer(out_ch),
+            nn.ReLU(True)
+        )
+
+    def forward(self, x):
+        x = self.up(x)
+        return x
+
+
+class Down(nn.Module):
+    
+    def __init__(self, in_ch, out_ch, norm_layer, use_bias):
+        super(Down, self).__init__()
+        self.down = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, kernel_size=3,
+                      stride=2, padding=1, bias=use_bias),
+            norm_layer(out_ch),
+            nn.ReLU(True)
+        )
+
+    def forward(self, x):
+        x = self.down(x)
+        return x
+
+
+class ResBlock(nn.Module):
+    
+    def __init__(self, dim, padding_type, norm_layer, use_dropout, use_bias):
+        super(ResBlock, self).__init__()
+        self.conv_block = self.build_conv_block(dim, padding_type, norm_layer, use_dropout, use_bias)
+
+    def build_conv_block(self, dim, padding_type, norm_layer, use_dropout, use_bias):
+        conv_block = []
+        p = 0
+        
+        if padding_type == 'reflect':
+            conv_block += [nn.ReflectionPad2d(1)]
+        elif padding_type == 'replicate':
+            conv_block += [nn.ReplicationPad2d(1)]
+        elif padding_type == 'zero':
+            p = 1
+        else:
+            raise NotImplementedError('padding [%s] is not implemented' % padding_type)
+
+        conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=p, bias=use_bias),
+                       norm_layer(dim),
+                       nn.ReLU(True)]
+        if use_dropout:
+            conv_block += [nn.Dropout(0.5)]
+
+        p = 0
+        if padding_type == 'reflect':
+            conv_block += [nn.ReflectionPad2d(1)]
+        elif padding_type == 'replicate':
+            conv_block += [nn.ReplicationPad2d(1)]
+        elif padding_type == 'zero':
+            p = 1
+        else:
+            raise NotImplementedError('padding [%s] is not implemented' % padding_type)
+
+
+        conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=p, bias=use_bias),
+                       norm_layer(dim)]
+
+        return nn.Sequential(*conv_block)
+
+
+    def forward(self, x):
+        out = x + self.conv_block(x)
+        return nn.ReLU(True)(out)
--- project/train.py
+++ project/train.py
@@ -9,117 +9,136 @@ import argparse
 
 from utils import mkdir
 from dataloader import SimulationDataSet
+from models import define_G
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler
+from torchsummary import summary
 
 
 import numpy as np
 
-def get_dataconf_file(args):
-    return args.model_type + '_dataconf.txt'
+# def get_dataconf_file(args):
+#     return args.model_type + '_dataconf.txt'
 
 
-parser = argparse.ArgumentParser(description='The training script of the flowPredict pytorch implementation')
-parser.add_argument('--data', dest='data_dir', required=True, help='Root directory of the generated data.')
-parser.add_argument('--model-name', dest='model_name', default='s_res_8', required=False, help='Name of the current model being trained')
-parser.add_argument('--config', dest='config', required=True, help='Configuration file for the system')
-parser.add_argument('--use-pressure', dest='use_pressure', required=False, action='store_true',
-                    default=False, help='Should the pressure field images to considered by the models')
-parser.add_argument('--model-type', dest='model_type', action='store', default='c', choices=['c', 'vd', 's', 'o'], required=False,
-                    help='Type of model to be build. \'c\' - baseline, \'vd\' - fluid viscosity and density, \'s\' - inflow speed, \'o\' - object')
-parser.add_argument('--cuda', dest='cuda', action='store_true', default=False, help='Should CUDA be used or not')
-parser.add_argument('--threads', dest='threads', type=int, default=4, help='Number of threads for data loader to use')
-parser.add_argument('--batch-size', dest='batch_size', type=int, default=4, help='Training batch size.')
-parser.add_argument('--test-train-split', dest='test_train_split', type=float, default=0.8, help='The percentage of the data to be used in the training set.')
-parser.add_argument('--shuffle', dest='shuffle', default=False, action='store_true', help='Should the training and testing data be shufffled.')
-parser.add_argument('--epochs', dest='epochs', type=int, default=5, help='Number of epochs for which the model will be trained')
-parser.add_argument('--seed', dest='seed', type=int, default=123, help='Random seed to use. Default=123')
+# parser = argparse.ArgumentParser(description='The training script of the flowPredict pytorch implementation')
+# parser.add_argument('--data', dest='data_dir', required=True, help='Root directory of the generated data.')
+# parser.add_argument('--model-name', dest='model_name', default='s_res_8', required=False, help='Name of the current model being trained')
+# parser.add_argument('--config', dest='config', required=True, help='Configuration file for the system')
+# parser.add_argument('--use-pressure', dest='use_pressure', required=False, action='store_true',
+#                     default=False, help='Should the pressure field images to considered by the models')
+# parser.add_argument('--model-type', dest='model_type', action='store', default='c', choices=['c', 'vd', 's', 'o'], required=False,
+#                     help='Type of model to be build. \'c\' - baseline, \'vd\' - fluid viscosity and density, \'s\' - inflow speed, \'o\' - object')
+# parser.add_argument('--cuda', dest='cuda', action='store_true', default=False, help='Should CUDA be used or not')
+# parser.add_argument('--threads', dest='threads', type=int, default=4, help='Number of threads for data loader to use')
+# parser.add_argument('--batch-size', dest='batch_size', type=int, default=4, help='Training batch size.')
+# parser.add_argument('--test-train-split', dest='test_train_split', type=float, default=0.8, help='The percentage of the data to be used in the training set.')
+# parser.add_argument('--shuffle', dest='shuffle', default=False, action='store_true', help='Should the training and testing data be shufffled.')
+# parser.add_argument('--epochs', dest='epochs', type=int, default=5, help='Number of epochs for which the model will be trained')
+# parser.add_argument('--seed', dest='seed', type=int, default=123, help='Random seed to use. Default=123')
+
+
+# print('===> Setting up basic structures ')
+
+# args = parser.parse_args()
+
+# print('--Model name:', args.model_name)
+
+# if args.cuda and not torch.cuda.is_available():
+#     raise Exception("No GPU found, please run without --cuda")
+
+# torch.manual_seed(args.seed)
+# if args.cuda:
+#     torch.cuda.manual_seed(args.seed)
+
+# device = torch.device("cuda:0" if args.cuda else "cpu")
 
 
 
-print('===> Setting up basic structures ')
+# with open(args.config, 'r') as config_handle:    
+#     config = json.load(config_handle)
 
-args = parser.parse_args()
+# model_type = args.model_type
+# test_train_split = args.test_train_split
+# batch_size = args.batch_size
+# shuffle_dataset = args.shuffle
+# num_epochs = args.epochs
+# threads = args.threads
+# random_seed = args.seed
+# model_name = args.model_name
 
-print('--Model name:', args.model_name)
+# print('--model type:', model_type)
+# print('--use pressure:', args.use_pressure)
+# print('--test-train split:', test_train_split)
+# print('--batch size:', batch_size)
+# print('--shuffle dataset:', shuffle_dataset)
+# print('--num epochs:', num_epochs)
+# print('--random seed:', random_seed)
+# print('--worker threads:', threads)
+# print('--cuda:', args.cuda)
+# print('--device:', device)
 
-if args.cuda and not torch.cuda.is_available():
-    raise Exception("No GPU found, please run without --cuda")
+# print('===> Loading datasets')
 
-torch.manual_seed(args.seed)
-if args.cuda:
-    torch.cuda.manual_seed(args.seed)
+# dataconf_file = get_dataconf_file(args)
+# dataset = SimulationDataSet(args.data_dir, dataconf_file, args)
+# dataset_size = len(dataset)
+# indices = list(range(dataset_size))
+# split = int(np.floor(test_train_split * dataset_size))
 
-device = torch.device("cuda:0" if args.cuda else "cpu")
-
-
-
-with open(args.config, 'r') as config_handle:    
-    config = json.load(config_handle)
-
-model_type = args.model_type
-test_train_split = args.test_train_split
-batch_size = args.batch_size
-shuffle_dataset = args.shuffle
-num_epochs = args.epochs
-threads = args.threads
-random_seed = args.seed
-model_name = args.model_name
-
-print('--model type:', model_type)
-print('--use pressure:', args.use_pressure)
-print('--test-train split:', test_train_split)
-print('--batch size:', batch_size)
-print('--shuffle dataset:', shuffle_dataset)
-print('--num epochs:', num_epochs)
-print('--random seed:', random_seed)
-print('--worker threads:', threads)
-print('--cuda:', args.cuda)
-print('--device:', device)
-
-print('===> Loading datasets')
-
-dataconf_file = get_dataconf_file(args)
-dataset = SimulationDataSet(args.data_dir, dataconf_file, args)
-dataset_size = len(dataset)
-indices = list(range(dataset_size))
-split = int(np.floor(test_train_split * dataset_size))
-
-if shuffle_dataset:
-    np.random.seed(random_seed)
-    np.random.shuffle(indices)
+# if shuffle_dataset:
+#     np.random.seed(random_seed)
+#     np.random.shuffle(indices)
     
-train_indices, test_indices = indices[:split], indices[split:]
+# train_indices, test_indices = indices[:split], indices[split:]
 
-print('--training samples count:', len(train_indices))
-print('--test samples count:', len(test_indices))
+# print('--training samples count:', len(train_indices))
+# print('--test samples count:', len(test_indices))
 
-train_sampler = SubsetRandomSampler(train_indices)
-test_sampler = SubsetRandomSampler(test_indices)
+# train_sampler = SubsetRandomSampler(train_indices)
+# test_sampler = SubsetRandomSampler(test_indices)
 
-train_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, sampler=train_sampler, shuffle=False, num_workers=threads)
-test_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, sampler=test_sampler, shuffle=False, num_workers=threads)
+# train_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, sampler=train_sampler, shuffle=False, num_workers=threads)
+# test_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, sampler=test_sampler, shuffle=False, num_workers=threads)
+
+device='cpu'
 
 print('===> Loading model')
+net_g = define_G(6, 6, 32, gpu_id=device).float()
 
-print('===> Starting the training loop')
+# summary(net_g, input_size=(6, 512, 512))
 
-# 
-for epoch in range(num_epochs):
-    for iteration, data in enumerate(train_loader, 1):
-        real_a, real_b = data[0].to(device), data[1].to(device)
-        # trainy things
+in_img = np.random.rand(1, 6, 512, 512)
+in_img = torch.from_numpy(in_img).float()
+
+for i in range(1, 20):
+    out_img  = net_g(in_img)
+    print(out_img.shape)
 
 
-    #updating learning rate
 
-    if epoch %   == 0:
-        mkdir("checkpoints")
-        mkdir(os.path.join("checkpoints", args.model_name))
-        # ...
-        print("Checkpoint saved to {}".format(os.path.join("checkpoints", args.model_name)))
+
+
+
+
+# print('===> Starting the training loop')
+
+# # 
+# for epoch in range(num_epochs):
+#     for iteration, data in enumerate(train_loader, 1):
+#         real_a, real_b = data[0].to(device), data[1].to(device)
+#         # trainy things
+
+
+#     #updating learning rate
+
+#     if epoch % 10  == 0:
+#         mkdir("checkpoints")
+#         mkdir(os.path.join("checkpoints", args.model_name))
+#         # ...
+#         print("Checkpoint saved to {}".format(os.path.join("checkpoints", args.model_name)))
 
